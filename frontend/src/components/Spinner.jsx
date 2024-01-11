cf2ecbf98b970bbe4f0870fcea18d73d4083aa08
Sru2003 <srushtipednekar15@gmail.com>
2024-01-11 09:09:36 +0530

    Login and Authentication Frontend Done

---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Spinner = () => {
+  return (
+    <div className='loadingSpinnerContainer'>
+        <div className="loadinSpinner"></div>
+    </div>
+  )
+}
+
+export default Spinner
